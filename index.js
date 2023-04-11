9882489f5444cb40c2a481bdc8caa2c23a1cb3ad
surya-teja12 <surya-teja12>
2023-04-11 10:08:46 +0000

    SuperOverLeague Solution

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="background-container">
+    <h1 className="text">Super Over League</h1>
+    <div className="img-container">
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        alt="RCB LOGO"
+      />
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        alt="RCB LOGO"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
